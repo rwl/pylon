dcc5b3d3259e80e8f628e3dec32b6dccb46b99eb
richard <richard@129418ec-a212-4327-99f4-75f8a8a7fd39>
2008-10-07 22:40:27 +0000

    Minor changes
    git-svn-id: http://pylon.eee.strath.ac.uk/svn/trunk@59 129418ec-a212-4327-99f4-75f8a8a7fd39

--- setup.py
+++ setup.py
@@ -22,11 +22,11 @@ setup(
     author_email="richard.lincoln@eee.strath.ac.uk",
     description="Open source power system and energy market analysis",
     url="https://pylon.eee.strath.ac.uk",
-    version="0.1.1",
+    version="0.1.8",
     entry_points={
         "gui_scripts": [
-            "pylon = pylon.plugin.main:main",
-            "pylon_vm = pylon.ui.model_view.main:main",
+            "envisage = pylon.plugin.main:main",
+            "pylon = pylon.ui.model_view.main:main",
             "pyreto = pylon.ui.model_view.pyreto:main"
         ]
     },
@@ -40,8 +40,8 @@ setup(
     include_package_data=True,
 #    exclude_package_data={"": ["*.ecore"]},
 #    package_data={"": ["*.txt", "*.rst", "*.png", "*.jpg", "*.ini"]},
-    package_dir={"": "src"},
-    packages=find_packages("src"),#exclude=["docs", "docs.*"]),
+#    package_dir={"": "src"},
+    packages=find_packages(),#"src"),#exclude=["docs", "docs.*"]),
     namespace_packages=[
         "enthought", "enthought.pyface", "enthought.pyface.ui",
         "enthought.plugins"
