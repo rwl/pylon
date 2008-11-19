a1ff419d3863284fa9ce2d887e5ef12d9e7b4c90
Richard W. Lincoln <r.w.lincoln@gmail.com>
2008-11-19 17:47:44 +0000

    Adding unit commitment routine and test case.

--- pylon/ui/routine/uc_view_model.py
+++ pylon/ui/routine/uc_view_model.py
@@ -1,5 +1,5 @@
 #------------------------------------------------------------------------------
-# Copyright (C) 2007 Richard W. Lincoln
+# Copyright (C) 2008 Richard W. Lincoln
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 # Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 #------------------------------------------------------------------------------
 
-""" DC power flow matrix visualisation """
+""" Unit commitment visualisation. """
 
 #------------------------------------------------------------------------------
 #  Imports:
@@ -26,37 +26,29 @@ from enthought.traits.api import HasTraits, Instance, Trait, Button
 from enthought.traits.ui.api import \
     Item, Group, View, InstanceEditor, HGroup, DropEditor, TabularEditor
 
-from cvxopt.base import matrix, spmatrix
-
 from pylon.network import Network
-from pylon.routine.api import DCPFRoutine
+from pylon.routine.api import UnitCommitmentRoutine
 from pylon.traits import Matrix, SparseMatrix
 
 #------------------------------------------------------------------------------
-#  "DCPFViewModel" class:
+#  "UnitCommitmentViewModel" class:
 #------------------------------------------------------------------------------
 
-class DCPFViewModel(HasTraits):
-    """ DC power flow matrix visualisation """
+class UnitCommitmentViewModel(HasTraits):
+    """ Unit commitment visualisation. """
 
     #--------------------------------------------------------------------------
     #  Trait definitions:
     #--------------------------------------------------------------------------
 
-    # The routine providing the matrices
-    routine = Instance(DCPFRoutine, ())
+    # The routine being visualised
+    routine = Instance(UnitCommitmentRoutine, ())
 
     # The network on which the routine is performed
     network = Instance(Network)
 
-    # Branch susceptance matrix
-    B = SparseMatrix(desc="Branch susceptance matrix")
-
-    # Branch source bus susceptance matrix
-    B_source = SparseMatrix(desc="Branch source bus susceptance matrix")
-
-    # A button that causes the routine to be solved
-    run = Button("Power Flow")
+    # A button that fires the routine
+    run = Button("Solve")
 
     # The default view
     traits_view = View(
@@ -65,46 +57,18 @@ class DCPFViewModel(HasTraits):
             editor=DropEditor(klass=Network)
         ),
         Item(name="run", style="simple", show_label=False),
-        Item(name="B"),
-        Item(name="B_source"),
-        id="pylon.routine.dc_pf.view",
-#        title="DC Engine",
+        id="pylon.routine.uc.view",
 #        icon=ImageResource("frame.ico", search_path=[IMAGE_LOCATION]),
-        resizable=True,
-        style="custom",
-        buttons=["OK"],
-        width=.2, #height = .4
+        resizable=True, style="custom",
+        buttons=["OK"], width=.2
     )
-
-    #--------------------------------------------------------------------------
-    #  "DCPFViewModel" interface:
-    #--------------------------------------------------------------------------
-
-    def _network_changed(self, new):
-        """ Sets the network attribute of the routine """
-
-        self.routine.network = new
-
-
-    def _run_fired(self):
-        """ Solves the routine and gets the resulting matrices """
-
-        self.routine.solve()
-        self.B = self.routine.B
-        self.B_source = self.routine.B_source
 
 #------------------------------------------------------------------------------
 #  Stand-alone call:
 #------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    from pylon.readwrite.api import read_matpower
-
-    data_file = "/home/rwl/python/aes/matpower_3.2/rwl_003.m"
-#    data_file = "/home/rwl/python/aes/matpower_3.2/case30.m"
-    n = read_matpower(data_file)
-
-    vm = DCPFViewModel(network=n)
+    vm = UnitCommitmentViewModel(network=Network())
     vm.configure_traits()
 
 # EOF -------------------------------------------------------------------------
