fddb3791017377569e0318142fcd80e42575f32f
Richard W. Lincoln <r.w.lincoln@gmail.com>
2009-01-03 20:24:21 +0000

    Adding graph attribute constructs.

--- godot/graph.py
+++ godot/graph.py
@@ -80,6 +80,15 @@ class Graph(HasTraits):
 
  #   _graph = Trait(graph.digraph)
 
+    # An ID is one of the following:
+    #  * Any string of alphabetic ([a-zA-Z\200-\377]) characters, underscores
+    #    ('_') or digits ([0-9]), not beginning with a digit;
+    #  * a number [-]?(.[0-9]+ | [0-9]+(.[0-9]*)? );
+    #  * any double-quoted string ("...") possibly containing escaped
+    #    quotes (\")1;
+    #  * an HTML string (<...>).
+    ID = Str
+
     nodes = List(Instance(Node))
 
     edges = List(Instance(Edge))
--- godot/parsing_util.py
+++ godot/parsing_util.py
@@ -38,7 +38,11 @@ from itertools import izip
 from pyparsing import \
     TokenConverter, oneOf, string, Literal, Group, Word, Optional, Combine, \
     sglQuotedString, dblQuotedString, restOfLine, nums, removeQuotes, Regex, \
-    OneOrMore
+    OneOrMore, hexnums, alphas, alphanums, CaselessLiteral
+
+#from enthought.enable.colors import color_table
+
+from godot.common import color_schemes
 
 #------------------------------------------------------------------------------
 #  "ToBoolean" class:
@@ -105,6 +109,8 @@ sign = oneOf("+ -")
 
 scolon = Literal(";").suppress()
 
+string = Word(alphanums)
+
 matlab_comment = Group(Literal('%') + restOfLine).suppress()
 psse_comment = Literal('@!') + Optional(restOfLine)
 
@@ -113,7 +119,10 @@ special_chars = string.replace(
     '!"#$%&\'()*,./:;<=>?@[\\]^_`{|}~', decimal_sep, ""
 )
 
-boolean = ToBoolean(ToInteger(Word("01", exact=1))).setName("bool")
+#boolean = ToBoolean(ToInteger(Word("01", exact=1))).setName("bool")
+true = CaselessLiteral("True") | And(integer, NotAny(Literal("0")))
+false = CaselessLiteral("False") | Literal("0")
+boolean = ToBoolean(true | false).setResultsName("boolean")
 
 integer = ToInteger(
     Combine(Optional(sign) + Word(nums))
@@ -133,6 +142,9 @@ real = ToFloat(
         Optional(oneOf("E e") + Word(nums))
     )
 ).setName("real")
+
+# TODO: Positive real number between zero and one.
+decimal = real
 
 positive_real = ToFloat(
     Combine(
@@ -169,6 +181,207 @@ quoted_string = Combine(
 #).setName("real_junk")
 #
 #q_string_junk = SkipTo(q_string).setName("q_string_junk")
+
+rect = real.setResultsName("llx") + real.setResultsName("lly") + \
+    real.setResultsName("urx") + real.setResultsName("ury")
+
+hex_color = Word(hexnums, exact=2) #TODO: Optional whitespace
+rgb = Literal("#") + hex_color.setResultsName("red") + \
+    hex_color.setResultsName("green") + hex_color.setResultsName("blue")
+rgba = rgb + hex_color.setResultsName("alpha")
+hsv = decimal.setResultsName("hue") + decimal.setResultsName("saturation") + \
+    decimal.setResultsName("value")
+color_name = Word(alphas)
+color = rgb | rgba | hsv | color_name
+
+cluster_mode = Literal("local") | Literal("global") | Literal("none")
+
+color_scheme = Or([Literal(scheme) for scheme in color_schemes])
+
+bb = rect.setResultsName("bb")
+bgcolor = color.setResultsName("bgcolor")
+center = boolean.setResultsName("center")
+charset = string.setResultsName("charset")
+clusterrank = cluster_mode.setResultsName("clusterrank")
+colorscheme = color_scheme.setResultsName("colorscheme")
+comment = string.setResultsName("comment")
+compound = boolean.setResultsName("compound")
+concentrate = boolean.setResultsName("concentrate")
+Damping = real.setResultsName("Damping")
+defaultdist = real.setResultsName("defaultdist")
+dim = Word("23456789", exact=1).setResultsName("dim")
+diredgeconstraints = (boolean |
+    CaselessLiteral("heir")).setResultsName("diredgeconstraints")
+dpi = real.setResultsName("dpi")
+epsilon = real.setResultsName("epsilon")
+esep = integer.setResultsName("esep")
+fontcolor = fontcolor_trait
+fontname = fontname_trait
+fontnames = Enum(
+    "svg", "ps", "gd", label="Font names",
+    desc="how basic fontnames are represented in SVG output"
+)
+fontpath = List(Directory, label="Font path")
+fontsize = fontsize_trait
+K = Float(0.3, desc="spring constant used in virtual physical model")
+label = label_trait
+labeljust = Trait(
+    "c", {"Centre": "c", "Right": "r", "Left": "l"},
+    desc="justification for cluster labels", label="Label justification"
+)
+labelloc = Trait(
+    "b", {"Bottom": "b", "Top":"t"},
+    desc="placement of graph and cluster labels",
+    label="Label location"
+)
+landscape = Bool(False, desc="rendering in landscape mode")
+layers = Str(desc="a linearly ordered list of layer names")
+layersep = Enum(
+    ":\t", "\t", " ", label="Layer separation",
+    desc="separator characters used to split layer names"
+)
+levelsgap = Float(
+    0.0, desc="strictness of level constraints in neato",
+    label="Levels gap"
+)
+lp = point_trait
+margin = Either(Float, pointf_trait, desc="x and y margins of canvas")
+maxiter = Int(desc="number of iterations used", label="Maximum iterations")
+mclimit = Float(
+    1.0, desc="Multiplicative scale factor used to alter the MinQuit "
+    "(default = 8) and MaxIter (default = 24) parameters used during "
+    "crossing minimization", label="Multiplicative scale factor"
+)
+mindist = Float(
+    1.0, desc="minimum separation between all nodes",
+    label="Minimum separation"
+)
+mode = Enum(
+    "major", "KK", "heir", "ipsep",
+    desc="Technique for optimizing the layout"
+)
+model = Enum(
+    "shortpath", "circuit", "subset",
+    desc="how the distance matrix is computed for the input graph"
+)
+mosek = Bool(False, desc="solve the ipsep constraints with MOSEK")
+nodesep = Float(
+    0.25, desc="minimum space between two adjacent nodes in the same rank",
+    label="Node separation"
+)
+nojustify = nojustify_trait
+normalize = Bool(
+    False, desc="If set, normalize coordinates of final layout so that "
+    "the first point is at the origin, and then rotate the layout so that "
+    "the first edge is horizontal"
+)
+nslimit = Float(
+    desc="iterations in network simplex applications",
+    label="x-coordinate limit"
+)
+nslimit1 = Float(
+    desc="iterations in network simplex applications",
+    label="Ranking limit"
+)
+ordering = Enum(
+    "out", "in", desc="If 'out' for a graph G, and n is a node in G, then "
+    "edges n->* appear left-to-right in the same order in which they are "
+    "defined. If 'in', the edges *->n appear left-to-right in the same "
+    "order in which they are defined for all nodes n."
+)
+outputorder = Enum(
+    "breadthfirst", "nodesfirst", "edgesfirst",
+    desc="order in which nodes and edges are drawn",
+    label="Output order"
+)
+overlap = Enum(
+    "True", "False", "scale", "scalexy", "prism", "compress", "vpsc", "ipsep",
+    desc="determines if and how node overlaps should be removed"
+)
+pack = Bool #Either(
+#        Bool, Int, desc="If true, each connected component of the graph is "
+#        "laid out separately, and then the graphs are packed tightly"
+#    )
+packmode = Enum(
+    "node", "cluster", "graph", label="Pack mode",
+    desc="granularity and method used for packing"
+)
+# the boundary of the drawn region.
+pad = Float(
+    0.0555, desc="how much to extend the drawing area around the minimal "
+    "area needed to draw the graph"
+)
+page = pointf_trait
+pagedir = Enum(
+    "BL", "BR", "TL", "TR", "RB", "RT", "LB", "LT",
+    desc="If the page attribute is set and applicable, this attribute "
+    "specifies the order in which the pages are emitted",
+    label="Page direction"
+)
+quantum = Float(
+    0.0, desc="If quantum > 0.0, node label dimensions will be rounded to "
+    "integral multiples of the quantum."
+)
+rank = Enum(
+    "same", "min", "source", "max", "sink",
+    desc="rank constraints on the nodes in a subgraph"
+)
+rankdir = Enum(
+    "TB", "LR", "BT", "RL", desc="direction of graph layout",
+    label="Rank direction"
+)
+ranksep = Float(
+    0.5, desc="In dot, this gives the desired rank separation.  In twopi, "
+    "specifies radial separation of concentric circles",
+    label="Rank separation"
+)
+ratio = Either(
+    Float, Enum("fill", "compress", "expand", "auto"),
+    desc="aspect ratio (drawing height/drawing width) for the drawing"
+)
+remincross = Bool(
+    False, desc="If true and there are multiple clusters, run cross "
+    "minimization a second", label="Re-cross minimization"
+)
+resolution = Alias("dpi", desc="a synonym for the dpi attribute")
+root = root_trait
+rotate = Range(0, 360, desc="drawing orientation")
+searchsize = Int(
+    30, desc="maximum number of edges with negative cut values to search "
+    "when looking for one with minimum cut value", label="Search size"
+)
+sep = Int(
+    4, desc="Fraction to increase polygons (multiply coordinates by "
+    "1 + sep) for purposes of determining overlap", label="Separation"
+)
+showboxes = showboxes_trait
+size = pointf_trait
+splines = Enum(
+    "True", "False", "", desc="how, and if, edges are represented"
+)
+start = start_trait
+stylesheet = Str(
+    desc="URL or pathname specifying an XML style sheet",
+    label="Style sheet"
+)
+target = target_trait
+truecolor = Bool(True)
+URL = Str(
+    desc="hyperlinks incorporated into device-dependent output",
+    label="URL"
+)
+viewport = Tuple(Float, Float, Float, Float, Float)
+#    Either(
+#        Tuple(Float, Float, Float, Float, Float),
+#        Tuple(Float, Float, Float, Str),
+#        desc="clipping window on final drawing"
+#    )
+
+voro_margin = Float(
+    0.05, desc="Factor to scale up drawing to allow margin for expansion "
+    "in Voronoi technique. dim' = (1+2*margin)*dim.",
+    label="Voronoi margin"
+)
 
 #------------------------------------------------------------------------------
 #  A convenient function for calculating a unique name given a list of
@@ -236,5 +449,9 @@ def windows(iterable, length=2, overlap=0, padding=True):
     if padding and results:
         results.extend(itertools.repeat(None, length-len(results)))
         yield results
+
+
+if __name__ == "__main__":
+    print hexnums
 
 # EOF -------------------------------------------------------------------------
