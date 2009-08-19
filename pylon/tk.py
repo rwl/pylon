7f8c5a6313a864ce913d052beb6fbf43d160093b
Richard W. Lincoln <r.w.lincoln@gmail.com>
2009-08-19 18:07:39 +0100

    Adding import/export menu items.

--- pylon/tk.py
+++ pylon/tk.py
@@ -5,8 +5,10 @@ import sys
 import logging
 
 from Tkinter import *
+from tkFileDialog import askopenfilename, asksaveasfilename
 
-from pylon.readwrite import MATPOWERReader, ReSTWriter
+from pylon.readwrite import \
+    MATPOWERReader, MATPOWERWriter, ReSTWriter, PSSEReader, PSATReader
 
 from pylon import \
     Network, DCPF, NewtonRaphson, FastDecoupled, DCOPF, ACOPF, UDOPF
@@ -19,16 +21,16 @@ CASE_30   = os.path.dirname(__file__) + "/test/data/case30pwl.m"
 
 class PylonTk:
     def __init__(self, master):
-        self.n = Network()
-
         self.root = master
 
         self.frame = Frame(master)
         self.frame.pack(expand=YES, fill=BOTH)
 
         self._init_menubar()
-        self._init_buttonbar()
+        self._init_toolbar()
         self._init_logframe()
+
+        self.on_new()
 
 
     def _init_menubar(self):
@@ -36,43 +38,63 @@ class PylonTk:
         self.root.config(menu=menu)
 
         filemenu = Menu(menu, tearoff=False)
+        filemenu.add_command(label="New", command=self.on_new)
+        filemenu.add_separator()
+        filemenu.add_command(label="Open...", command=self.on_open)
         menu.add_cascade(label="Case", menu=filemenu)
+
         presetmenu = Menu(filemenu, tearoff=False)
         filemenu.add_cascade(label='Preset', menu=presetmenu)
         presetmenu.add_command(label="6 bus", command=self.on_6_bus)
         presetmenu.add_command(label="30 bus", command=self.on_30_bus)
         filemenu.add_separator()
+
+        filemenu.add_command(label="Save As...", command=self.on_save_as)
+        filemenu.add_separator()
+
+        importmenu = Menu(filemenu, tearoff=False)
+        filemenu.add_cascade(label='Import', menu=importmenu)
+        importmenu.add_command(label="Pickle", command=self.on_unpickle)
+        importmenu.add_command(label="PSS/E", command=self.on_psse)
+        importmenu.add_command(label="PSAT", command=self.on_psat)
+
+        exportmenu = Menu(filemenu, tearoff=False)
+        filemenu.add_cascade(label='Export', menu=exportmenu)
+        exportmenu.add_command(label="Pickle", command=self.on_pickle)
+        exportmenu.add_command(label="Excel", command=self.on_excel)
+        exportmenu.add_command(label="CSV", command=self.on_csv)
+        exportmenu.add_command(label="ReST", command=self.on_rest)
+        filemenu.add_separator()
+
         filemenu.add_command(label="Exit", command=self.on_exit,
                              accelerator="Alt-X")
         self.root.bind('<Alt-x>', self.on_exit)
 
         pfmenu = Menu(menu, tearoff=False)
         menu.add_cascade(label="Power Flow", menu=pfmenu)
-        pfmenu.add_command(label="DC", command=self.on_dcpf)
+        pfmenu.add_command(label="DC PF", command=self.on_dcpf)
         pfmenu.add_command(label="Newton-Raphson", command=self.on_newton)
         pfmenu.add_command(label="Fast Decoupled", command=self.on_fd)
 
         opfmenu = Menu(menu, tearoff=False)
         menu.add_cascade(label="OPF", menu=opfmenu)
-        opfmenu.add_command(label="DC", command=self.on_dcopf)
-        opfmenu.add_command(label="AC", command=self.on_acopf)
-        opfmenu.add_command(label="DC (UD)", command=self.on_duopf)
-        opfmenu.add_command(label="AC (UD)", command=self.on_uopf)
+        opfmenu.add_command(label="DC OPF", command=self.on_dcopf)
+        opfmenu.add_command(label="AC OPF", command=self.on_acopf)
+        opfmenu.add_command(label="DC (UD) OPF", command=self.on_duopf)
+        opfmenu.add_command(label="AC (UD) OPF", command=self.on_uopf)
 
 
-    def _init_buttonbar(self):
-        buttonbar = Frame(self.frame)
-        buttonbar.pack(side=LEFT, fill=Y)
-        Button(buttonbar, text="Summary",
+    def _init_toolbar(self):
+        toolbar = Frame(self.frame)
+        toolbar.pack(side=LEFT, fill=Y)
+        Button(toolbar, text="Summary",
                command=self.on_summary).pack(fill=X)
-        Button(buttonbar, text="Bus Data",
+        Button(toolbar, text="Bus",
                command=self.on_bus_info).pack(fill=X)
-        Button(buttonbar, text="Branch Data",
+        Button(toolbar, text="Branch",
                command=self.on_branch_info).pack(fill=X)
-        Button(buttonbar, text="Generator Data",
+        Button(toolbar, text="Generator",
                command=self.on_generator_info).pack(fill=X)
-        Button(buttonbar, text="Load Data",
-               command=self.on_load_info).pack(fill=X)
 
 
     def _init_logframe(self):
@@ -85,6 +107,80 @@ class PylonTk:
 
         self.ui_log.level.set(logger.getEffectiveLevel())
 
+
+    def on_new(self):
+        self.n = Network()
+
+
+    def on_open(self):
+        ftypes = [("MATLAB file", ".m"), ("All files", "*")]
+        filename = askopenfilename(filetypes=ftypes, defaultextension='.m')
+        if filename:
+            self.n = MATPOWERReader().read(filename)
+
+
+    def on_6_bus(self):
+        self.n = MATPOWERReader().read(CASE_6_WW)
+
+
+    def on_30_bus(self):
+        self.n = MATPOWERReader().read(CASE_30)
+
+
+    def on_save_as(self):
+        filename = asksaveasfilename(filetypes=[("MATLAB file", ".m")])
+        if filename:
+            MATPOWERWriter().write(self.n, filename)
+
+    # Import handlers ---------------------------------------------------------
+
+    def on_unpickle(self):
+        ftypes = [("Pickle file", ".pkl"), ("All files", "*")]
+        filename = askopenfilename(filetypes=ftypes, defaultextension='.pkl')
+        if filename:
+            self.n = PickleReader().read(filename)
+
+
+    def on_psse(self):
+        ftypes = [("PSS/E file", ".raw"), ("All files", "*")]
+        filename = askopenfilename(filetypes=ftypes, defaultextension='.raw')
+        if filename:
+            self.n = PSSEReader().read(filename)
+
+
+    def on_psat(self):
+        ftypes = [("PSAT file", ".m"), ("All files", "*")]
+        filename = askopenfilename(filetypes=ftypes, defaultextension='.m')
+        if filename:
+            self.n = PSATReader().read(filename)
+
+    # Export handlers ---------------------------------------------------------
+
+    def on_pickle(self):
+        filename = asksaveasfilename(filetypes=[("Pickle file", ".pkl")])
+        if filename:
+            PickleWriter().write(self.n, filename)
+
+
+    def on_excel(self):
+        filename = asksaveasfilename(filetypes=[("Excel file", ".xls")])
+        if filename:
+            ExcelWriter().write(self.n, filename)
+
+
+    def on_csv(self):
+        filename = asksaveasfilename(filetypes=[("CSV file", ".csv")])
+        if filename:
+            CSVWriter().write(self.n, filename)
+
+
+    def on_rest(self):
+        ftypes = [("ReStructuredText file", ".rst")]
+        filename = asksaveasfilename(filetypes=ftypes)
+        if filename:
+            ReSTWriter().write(self.n, filename)
+
+    # -------------------------------------------------------------------------
 
     def on_summary(self):
         writer = ReSTWriter()
@@ -103,18 +199,6 @@ class PylonTk:
 
     def on_generator_info(self):
         ReSTWriter().write_generator_data(self.n, self.ui_log)
-
-
-    def on_load_info(self):
-        ReSTWriter().write_load_data(self.n, self.ui_log)
-
-
-    def on_6_bus(self):
-        self.n = MATPOWERReader().read(CASE_6_WW)
-
-
-    def on_30_bus(self):
-        self.n = MATPOWERReader().read(CASE_30)
 
 
     def on_dcpf(self):
@@ -152,25 +236,38 @@ class PylonTk:
 
 class UILog:
     def __init__(self, master):
-        logframe = Frame(master)
-        logframe.pack(expand=YES, fill=BOTH)
+        self.master = master
+
+        self._init_text()
+        self._init_levels()
+
+
+    def _init_text(self):
+        logframe = Frame(self.master)
+
+        logframe.grid_rowconfigure(0, weight=1)
+        logframe.grid_columnconfigure(0, weight=1)
+
+        xscrollbar = Scrollbar(logframe, orient=HORIZONTAL)
+        xscrollbar.grid(row=1, column=0, sticky=E+W)
 
         yscrollbar = Scrollbar(logframe)
-        xscrollbar = Scrollbar(logframe, orient=HORIZONTAL)
+        yscrollbar.grid(row=0, column=1, sticky=N+S)
 
-        self.log = Text(logframe, background="white", wrap=NONE,
+        log = self.log = Text(logframe, wrap=NONE, background="white",
                         xscrollcommand=xscrollbar.set,
                         yscrollcommand=yscrollbar.set)
 
-        yscrollbar.pack(side=RIGHT, fill=Y)
-        xscrollbar.pack(side=BOTTOM, fill=X)
+        log.grid(row=0, column=0, sticky=N+S+E+W)
 
-        self.log.pack(expand=YES, fill=BOTH)
+        xscrollbar.config(command=log.xview)
+        yscrollbar.config(command=log.yview)
 
-        yscrollbar.config(command=self.log.yview)
-        xscrollbar.config(command=self.log.xview)
+        logframe.pack(expand=YES, fill=BOTH)
 
-        loglevels = Frame(master)
+
+    def _init_levels(self):
+        loglevels = Frame(self.master)
         loglevels.pack(fill=X)
 
         level = self.level = IntVar()
@@ -211,6 +308,8 @@ class UILog:
 
 def main():
     root = Tk()
+    root.minsize(300, 300)
+#    root.geometry("666x666")
     root.title('PYLON')
     app = PylonTk(root)
     root.mainloop()
