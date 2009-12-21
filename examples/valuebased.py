2ac22ac09632716c94a3d267920faed4633fda05
Richard W. Lincoln <r.w.lincoln@gmail.com>
2009-12-21 11:37:58 +0000

    Adding parameters for publication quality plots using TeX.

--- examples/valuebased.py
+++ examples/valuebased.py
@@ -4,6 +4,13 @@ __author__ = 'Richard Lincoln, r.w.lincoln@gmail.com'
 Reinforcement Learning algorithms (SARSA, Q, Q(lambda)) in a partially
 observable MDP energy market task. """
 
+#import matplotlib
+#matplotlib.use('WXAgg')
+#matplotlib.rc('font', **{'family': 'sans-serif',
+#                         'sans-serif': ['Computer Modern Sans serif']})
+#matplotlib.rc('font', **{'family': 'serif', 'serif': ['Computer Modern Roman']})
+#matplotlib.rc('text', usetex=True)
+
 import sys
 import logging
 import time
@@ -20,6 +27,23 @@ from pybrain.rl.learners.valuebased import ActionValueTable#, ActionValueNetwork
 from pybrain.rl.learners import Q, QLambda, SARSA #@UnusedImport
 from pybrain.rl.explorers import BoltzmannExplorer #@UnusedImport
 from pybrain.tools.plotting import MultilinePlotter
+
+# Set up publication quality graphs.
+#fig_width_pt = 246.0  # Get this from LaTeX using \showthe\columnwidth
+#inches_per_pt = 1.0 / 72.27               # Convert pt to inch
+#golden_mean = (pylab.sqrt(5) - 1.0) / 2.0 # Aesthetic ratio
+#fig_width = fig_width_pt * inches_per_pt  # width in inches
+#fig_height = fig_width * golden_mean      # height in inches
+#fig_size = [fig_width, fig_height]
+#params = {'backend': 'ps',
+#          'axes.labelsize': 10,
+#          'text.fontsize': 10,
+#          'legend.fontsize': 10,
+#          'xtick.labelsize': 8,
+#          'ytick.labelsize': 8,
+#          'text.usetex': True,
+#          'figure.figsize': fig_size}
+#pylab.rcParams.update(params)
 
 # Set up the logger.
 logger = logging.getLogger()
@@ -59,18 +83,16 @@ agent2 = LearningAgent(module2, SARSA())
 exp = MarketExperiment([task1, task2], [agent1, agent2], mkt)
 
 # Prepare for plotting.
-pylab.figure()
 pylab.ion()
-pl = MultilinePlotter(autoscale=1.2, xlim=[0, 24], ylim=[0, 1])
+pl = MultilinePlotter(autoscale=1.1, xlim=[0, 24], ylim=[0, 1])
 pl.setLineStyle(linewidth=2)
 pl.setLegend([agent1.name], loc='upper right')
 
 # Execute interactions with the enironment in batch mode.
 t0 = time.time()
 x = 0
-batch = 24
-while x < 240:
-    x += batch
+batch = 5
+while x <= 1200:
     exp.doInteractions(batch)
 
     # Plot the progress of the agents.
@@ -87,9 +109,10 @@ while x < 240:
     agent1.reset()
     agent2.learn()
     agent2.reset()
-
-pl.show("Periods", "Reward", "Value-based Reward", popup=True)
+    x += batch
 
 logger.info("Example completed in %.3fs" % (time.time() - t0))
+
+#pl.show("Periods", "Reward", "Value-based Reward", popup=True)
 
 time.sleep(6)
