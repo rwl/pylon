5e5d836b400c290b182ce477412b1f2e4fc0b39d
Richard W. Lincoln <r.w.lincoln@gmail.com>
2010-01-07 13:10:12 +0000

    Adding constructor to CVXOPTSolver.

--- pylon/opf.py
+++ pylon/opf.py
@@ -745,9 +745,9 @@ class PDIPMSolver(Solver):
     """
 
     def __init__(self, om, flow_lim="S"):
-        """ Initialises a new DCOPFSolver instance.
+        """ Initialises a new PDIPMSolver instance.
         """
-        super(DCOPFSolver, self).__init__(om)
+        super(PDIPMSolver, self).__init__(om)
 
         # Quantity to limit for branch flow constraints ("S", "P" or "I").
         self.flow_lim = flow_lim
@@ -1067,56 +1067,39 @@ class CVXOPTSolver(Solver):
     """ Solves AC optimal power flow using convex optimization.
     """
 
+    def __init__(self, om, flow_lim="S"):
+        """ Initialises a new CVXOPTSolver instance.
+        """
+        super(CVXOPTSolver, self).__init__(om)
+
+        # Quantity to limit for branch flow constraints ("S", "P" or "I").
+        self.flow_lim = flow_lim
+
+
     def solve(self):
+        j = 0 + 1j
+        case = self.om.case
+        base_mva = case.base_mva
         # Unpack the OPF model.
         buses, branches, generators = self._unpack_model(self.om)
         # Compute problem dimensions.
+        ng = len(generators)
         ipol, ipwl, nb, nl, nw, ny, nxyz = self._dimension_data(buses,
                                                                 branches,
                                                                 generators)
-        # Split the constraints in equality and inequality.
-        Aeq, beq, Aieq, bieq = self._split_constraints(self.om)
-
-        # Get the function that evaluates the objective and nonlinear
-        # constraint functions.
-        F = self._objective_and_nonlinear_constraint_function()
-
-        # Solve the convex optimization problem.
-        result = self._run_opf(F, Aieq, bieq, Aeq, beq)
-
-
-    def _objective_and_nonlinear_constraint_function(self, om, buses, branches,
-                                                     generators, nb, nl, ng,
-                                                     base_mva):
-        """ Returns a function that evaluates the objective and nonlinear
-            constraint functions.
-        """
-        j = 0 + 1j
-
-        # Optimisation variables.
-        Va = om.get_var("Va")
-        Vm = om.get_var("Vm")
-        Pg = om.get_var("Pg")
-        Qg = om.get_var("Qg")
-
         # The number of non-linear equality constraints.
         neq = 2 * nb
         # The number of control variables.
         nc = 2 * nb + 2 * ng
 
-        # Definition of indexes for the optimisation variable vector.
-        # Voltage phase angle.
-#        ph_base = 0
-#        ph_end  = ph_base + nb - 1;
-#        # Voltage amplitude.
-#        v_base  = ph_end + 1
-#        v_end   = v_base + nb - 1
-#        # Active generation.
-#        pg_base = v_end + 1
-#        pg_end  = pg_base + ng - 1
-#        # Reactive generation.
-#        qg_base = pg_end + 1
-#        qg_end  = qg_base + ng - 1
+        # Split the constraints in equality and inequality.
+        Aeq, beq, Aieq, bieq = self._split_constraints(self.om)
+
+        # Optimisation variables.
+        Va = self.om.get_var("Va")
+        Vm = self.om.get_var("Vm")
+        Pg = self.om.get_var("Pg")
+        Qg = self.om.get_var("Qg")
 
 
         def F(x=None, z=None):
@@ -1126,7 +1109,7 @@ class CVXOPTSolver(Solver):
                 # Number of non-linear constraints.
                 m = neq
 
-                x0 = matrix(0., (om.get_var_N(), 1))
+                x0 = matrix(0., (nxyz, 1))
                 x0[Va.i1:Va.iN] = 0.0
                 x0[Vm.i1:Vm.iN] = 1.0
                 x0[Pg.i1:Pg.iN] = [g.p_min + g.p_max / 2 / base_mva
@@ -1161,7 +1144,7 @@ class CVXOPTSolver(Solver):
                 der = polyder( list(g.p_cost) )
                 df_dPgQg[i] = polyval(der, g.p) * base_mva
 
-            df0= matrix([matrix(0.0, (v_end, 1)), df_dPgQg])
+            df0= matrix([matrix(0.0, (Vm.iN, 1)), df_dPgQg])
 
             # Evaluate nonlinear equality constraints -------------------------
 
@@ -1170,8 +1153,8 @@ class CVXOPTSolver(Solver):
                         for v in buses])
 
             # Bus voltage vector.
-            v_angle = x[ph_base:ph_end + 1]
-            v_magnitude = x[v_base:v_end]
+            v_angle = x[Va.i1:Va.iN + 1]
+            v_magnitude = x[Vm.i1:Vm.iN + 1]
 #            Va0r = Va0 * pi / 180 #convert to radians
             v = mul(v_magnitude, exp(j * v_angle)) #element-wise product
 
@@ -1201,7 +1184,7 @@ class CVXOPTSolver(Solver):
             # Evaluate partial derivatives of constraints ---------------------
 
             # Partial derivative of injected bus power
-            dS_dVm, dS_dVa = dSbus_dV(Y, v) # w.r.t voltage
+            dS_dVm, dS_dVa = case.dSbus_dV(Y, v) # w.r.t voltage
             pv_idxs = matrix([buses.index(bus) for bus in buses])
             dS_dPg = spmatrix(-1, pv_idxs, range(ng)) # w.r.t Pg
             dS_dQg = spmatrix(-j, pv_idxs, range(ng)) # w.r.t Qg
@@ -1218,11 +1201,11 @@ class CVXOPTSolver(Solver):
 
             # Partial derivative of branch power flow w.r.t voltage.
             dSf_dVa, dSt_dVa, dSf_dVm, dSt_dVm, s_from, s_to = \
-                dSbr_dV(branches, Yfrom, Yto, v)
+                case.dSbr_dV(Yfrom, Yto, v)
 
             # Magnitude of complex power flow.
             df_dVa, dt_dVa, df_dVm, dt_dVm = \
-                dAbr_dV(dSf_dVa, dSf_dVm, dSt_dVa, dSt_dVm, s_from, s_to)
+                case.dAbr_dV(dSf_dVa, dSf_dVm, dSt_dVa, dSt_dVm, s_from, s_to)
 
             # Transposed Jacobian of branch power flow inequality constraints.
             dfk_ieq = matrix([matrix([df_dVa, df_dVm]),
@@ -1243,25 +1226,18 @@ class CVXOPTSolver(Solver):
                 # TODO: Implement reactive power costs.
                 d2f_d2Pg[i] = polyval(der, g.p) * base_mva
 
-            i = matrix(range(pg_base, qg_end + 1)).T
+            i = matrix(range(Pg.i1, Qg.iN + 1)).T
             H = spmatrix(matrix([d2f_d2Pg, d2f_d2Qg]), i, i)
 
             return f, df, H
 
-        return F
-
-
-    def _run_opf(self, F, G, h, A, b):
-        """ Solves the convex optimal power flow problem.
-        """
         # cp(F, G=None, h=None, dims=None, A=None, b=None, kktsolver=None)
         #
         #     minimize    f0(x)
         #     subject to  fk(x) <= 0, k = 1, ..., mnl
         #                 G*x   <= h
         #                 A*x   =  b.
-        dims = None
-        solution = solvers.cp(F, G, h, dims, A, b)
+        solution = solvers.cp(F, G=Aieq, h=bieq, dims=None, A=Aeq, b=beq)
 
         return solution
 
