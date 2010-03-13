a90fa6a05719cfdf7a2ff10c28c335590b858230
Richard Lincoln <r.w.lincoln@gmail.com>
2010-03-13 18:28:10 +0000

    Adding PF example.

---
+++ examples/pf.py
@@ -0,0 +1,17 @@
+__author__ = 'Richard Lincoln, r.w.lincoln@gmail.com'
+
+""" This example demonstrates how to solve a power flow problem using data
+in PSS/E Raw format. """
+
+import sys
+import pylon
+
+# Data files format if recognised according to file extension.
+case = pylon.Case.load("bench30.raw")
+
+# Pass the case to the solver and solve.
+pylon.NewtonPF(case).solve()
+#pylon.FastDecoupledPF(case, method="XB").solve()
+
+# Print a report to screen.
+case.save_rst(sys.stdout)
