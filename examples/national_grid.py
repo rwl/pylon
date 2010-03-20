a85ecb732603281af8502b118058677662bd86d4
Richard W. Lincoln <r.w.lincoln@gmail.com>
2010-03-20 16:20:43 +0000

    Adding UK transmission grid example.

---
+++ examples/national_grid.py
@@ -0,0 +1,157 @@
+import pylon
+
+# SPT 400kV
+hunterston = pylon.Bus("HUER Hunterston", v_base=400.0)
+inverkip = pylon.Bus("INKI Inverkip", v_base=400.0)
+devol_moor = pylon.Bus("DEVM Devol Moor", v_base=400.0)
+kilmarnock_south = pylon.Bus("KILS Kilmarnock South", v_base=400.0)
+strathaven = pylon.Bus("STHA Strathaven", v_base=400.0)
+coalburn = pylon.Bus("COAL Coalburn", v_base=400.0)
+elvanfoot = pylon.Bus("ELVA Elvanfoot", v_base=400.0)
+gretna = pylon.Bus("GRNA Gretna", v_base=400.0)
+straiton = pylon.Bus("SMEA (Straiton)", v_base=400.0)
+cryr = pylon.Bus("CRYR", v_base=400.0)
+torness = pylon.Bus("TORN Torness", v_base=400.0)
+cockenzie = pylon.Bus("COCK Cockenzie", v_base=400.0)
+eccles = pylon.Bus("ECCL Eccles", v_base=400.0)
+
+# NGET
+harker = pylon.Bus("Harker", v_base=400.0)
+stella_west = pylon.Bus("Stella West", v_base=400.0)
+hutton = pylon.Bus("Hutton", v_base=400.0)
+heysham = pylon.Bus("Heysham", v_base=400.0)
+penwortham = pylon.Bus("Penwortham", v_base=400.0)
+padham = pylon.Bus("Padham", v_base=400.0)
+kearsley = pylon.Bus("Kearsley", v_base=400.0)
+dains = pylon.Bus("Daines", v_base=400.0)
+macclesfield = pylon.Bus("Macclesfield", v_base=400.0)
+deeside = pylon.Bus("Deeside", v_base=400.0)
+frodsham = pylon.Bus("Frodsham", v_base=400.0)
+dinorwig = pylon.Bus("Dinorwig", v_base=400.0)
+trawsfynydd = pylon.Bus("Trawsfynydd", v_base=400.0)
+legacy = pylon.Bus("Legacy", v_base=400.0)
+cellarhead = pylon.Bus("Cellarhead", v_base=400.0)
+drakelow = pylon.Bus("Drakelow", v_base=400.0)
+willington_east = pylon.Bus("Willington East", v_base=400.0)
+ironbridge = pylon.Bus("Ironbridge", v_base=400.0)
+rugeley = pylon.Bus("Rugeley", v_base=400.0)
+hams_hall = pylon.Bus("Hams Hall", v_base=400.0)
+feckenham = pylon.Bus("Feckenham", v_base=400.0)
+walham = pylon.Bus("Walham", v_base=400.0)
+pembroke = pylon.Bus("Pembroke", v_base=400.0)
+swansea_north = pylon.Bus("Swansea North", v_base=400.0)
+cilfynydd = pylon.Bus("Cilfynydd", v_base=400.0)
+imperial_park = pylon.Bus("Imperial Park", v_base=400.0)
+seabank = pylon.Bus("Seabank", v_base=400.0)
+melksham = pylon.Bus("Melksham", v_base=400.0)
+minety = pylon.Bus("Minety", v_base=400.0)
+cowley = pylon.Bus("Cowley", v_base=400.0)
+didcot = pylon.Bus("didcot", v_base=400.0)
+bramley = pylon.Bus("Bramley", v_base=400.0)
+fleet = pylon.Bus("Fleet", v_base=400.0)
+lovedean = pylon.Bus("Lovedean", v_base=400.0)
+botley_wood = pylon.Bus("Botley Wood", v_base=400.0)
+fawley = pylon.Bus("Fawley", v_base=400.0)
+marchwood = pylon.Bus("Marchwood", v_base=400.0)
+nursling = pylon.Bus("Nursling", v_base=400.0)
+mannington = pylon.Bus("Mannington", v_base=400.0)
+chickerell = pylon.Bus("Chickerell", v_base=400.0)
+axminster = pylon.Bus("Axminster", v_base=400.0)
+exeter = pylon.Bus("Exeter", v_base=400.0)
+langage = pylon.Bus("Langage", v_base=400.0)
+landulph = pylon.Bus("Landulph", v_base=400.0)
+indian_queens = pylon.Bus("Indian Queens", v_base=400.0)
+taunton = pylon.Bus("Taunton", v_base=400.0)
+hinkley_point = pylon.Bus("Hinkley Point", v_base=400.0)
+bolney = pylon.Bus("Bolney", v_base=400.0)
+ninfield = pylon.Bus("ninfield", v_base=400.0)
+dugeness = pylon.Bus("Dugeness", v_base=400.0)
+sellindge = pylon.Bus("Sellindge", v_base=400.0)
+canterbury_north = pylon.Bus("Canterbury North", v_base=400.0)
+kemsley = pylon.Bus("Kemsley", v_base=400.0)
+rowdown = pylon.Bus("Rowdown", v_base=400.0)
+beddington = pylon.Bus("Beddington", v_base=400.0)
+littlebrook = pylon.Bus("Littlebrook", v_base=400.0)
+grain = pylon.Bus("Grain", v_base=400.0)
+kingsnorth = pylon.Bus("Kingsnorth", v_base=400.0)
+northfleet_east = pylon.Bus("Northfleet East", v_base=400.0)
+barking = pylon.Bus("Barking", v_base=400.0)
+west_ham = pylon.Bus("Westham", v_base=400.0)
+city_road = pylon.Bus("City Road", v_base=400.0)
+unknown = pylon.Bus(v_base=400.0)
+coryton = pylon.Bus("Coryton", v_base=400.0)
+rayleigh_main = pylon.Bus("Rayleigh Main", v_base=400.0)
+bramford = pylon.Bus("Bramford", v_base=400.0)
+sizewell = pylon.Bus("Sizewell", v_base=400.0)
+norwich_main = pylon.Bus("Norwich Main", v_base=400.0)
+pelham = pylon.Bus("Pelham", v_base=400.0)
+wymondley_main = pylon.Bus("Wymondley Main", v_base=400.0)
+sundon = pylon.Bus("Sundon", v_base=400.0)
+east_claydon = pylon.Bus("East Claydon", v_base=400.0)
+enderby = pylon.Bus("Enderby", v_base=400.0)
+ratcliffe_on_soar = pylon.Bus("Ratcliffe on Soar", v_base=400.0)
+grendon = pylon.Bus("Grendon", v_base=400.0)
+staythorpe = pylon.Bus("Staythorpe", v_base=400.0)
+cottam = pylon.Bus("Cottam", v_base=400.0)
+high_marnham = pylon.Bus("", v_base=400.0)
+eaton_socon = pylon.Bus("Eaton Socon", v_base=400.0)
+burwell_main = pylon.Bus("Burwell Main", v_base=400.0)
+walpole = pylon.Bus("Walpole", v_base=400.0)
+spalding_north = pylon.Bus("Spalding North", v_base=400.0)
+west_burton = pylon.Bus("West Burton", v_base=400.0)
+keadby = pylon.Bus("Keadby", v_base=400.0)
+grimsby_west = pylon.Bus("Grimsby West", v_base=400.0)
+south_humber_bank = pylon.Bus("South Humber Bank", v_base=400.0)
+killingholme = pylon.Bus("Killingholme", v_base=400.0)
+humber_refinery = pylon.Bus("Humber Refinery", v_base=400.0)
+creyke_beck = pylon.Bus("Creyke Beck", v_base=400.0)
+thornton = pylon.Bus("Thorton", v_base=400.0)
+drax = pylon.Bus("Drax", v_base=400.0)
+eggsborough = pylon.Bus("Eggsborough", v_base=400.0)
+thorpe_marsh = pylon.Bus("Thorpe Marsh", v_base=400.0)
+brinsworth = pylon.Bus("Brinsworth", v_base=400.0)
+stocksbridge = pylon.Bus("Stocksbridge", v_base=400.0)
+monk_fryston = pylon.Bus("Monk Fryston", v_base=400.0)
+osbaldwick = pylon.Bus("Osbaldwick", v_base=400.0)
+norton = pylon.Bus("Norton", v_base=400.0)
+lackenby = pylon.Bus("Lackenby", v_base=400.0)
+
+bus = pylon.Bus("", v_base=400.0)
+
+
+spt_400 =[
+    pylon.Branch(hunterston, inverkip),
+    pylon.Branch(inverkip, devol_moor),
+    pylon.Branch(hunterston, kilmarnock_south),
+    pylon.Branch(kilmarnock_south, strathaven),
+    pylon.Branch(strathaven, coalburn),
+    pylon.Branch(coalburn, elvanfoot),
+    pylon.Branch(elvanfoot, gretna),
+    pylon.Branch(strathaven, straiton),
+    pylon.Branch(straiton, torness),
+    pylon.Branch(cockenzie, eccles)
+]
+
+ngt_400 = [
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+    pylon.Branch(),
+]
