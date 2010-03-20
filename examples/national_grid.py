31c74b446e2abcb89befad3fd4f943ea28f8b785
Richard W. Lincoln <r.w.lincoln@gmail.com>
2010-03-20 17:41:44 +0000

    Adding NGT 400kV branches.

--- examples/national_grid.py
+++ examples/national_grid.py
@@ -18,7 +18,8 @@ eccles = pylon.Bus("ECCL Eccles", v_base=400.0)
 # NGET
 harker = pylon.Bus("Harker", v_base=400.0)
 stella_west = pylon.Bus("Stella West", v_base=400.0)
-hutton = pylon.Bus("Hutton", v_base=400.0)
+hutton_1 = pylon.Bus("Hutton 1", v_base=400.0)
+hutton_2 = pylon.Bus("Hutton 2", v_base=400.0)
 heysham = pylon.Bus("Heysham", v_base=400.0)
 penwortham = pylon.Bus("Penwortham", v_base=400.0)
 padham = pylon.Bus("Padham", v_base=400.0)
@@ -28,6 +29,8 @@ macclesfield = pylon.Bus("Macclesfield", v_base=400.0)
 deeside = pylon.Bus("Deeside", v_base=400.0)
 frodsham = pylon.Bus("Frodsham", v_base=400.0)
 dinorwig = pylon.Bus("Dinorwig", v_base=400.0)
+pentir = pylon.Bus("Pentir", v_base=400.0)
+wylfa = pylon.Bus("Wlyfa", v_base=400.0)
 trawsfynydd = pylon.Bus("Trawsfynydd", v_base=400.0)
 legacy = pylon.Bus("Legacy", v_base=400.0)
 cellarhead = pylon.Bus("Cellarhead", v_base=400.0)
@@ -38,6 +41,7 @@ rugeley = pylon.Bus("Rugeley", v_base=400.0)
 hams_hall = pylon.Bus("Hams Hall", v_base=400.0)
 feckenham = pylon.Bus("Feckenham", v_base=400.0)
 walham = pylon.Bus("Walham", v_base=400.0)
+rassau = pylon.Bus("Rassau", v_base=400.0)
 pembroke = pylon.Bus("Pembroke", v_base=400.0)
 swansea_north = pylon.Bus("Swansea North", v_base=400.0)
 cilfynydd = pylon.Bus("Cilfynydd", v_base=400.0)
@@ -55,11 +59,13 @@ fawley = pylon.Bus("Fawley", v_base=400.0)
 marchwood = pylon.Bus("Marchwood", v_base=400.0)
 nursling = pylon.Bus("Nursling", v_base=400.0)
 mannington = pylon.Bus("Mannington", v_base=400.0)
-chickerell = pylon.Bus("Chickerell", v_base=400.0)
+chickerell_1 = pylon.Bus("Chickerell 1", v_base=400.0)
+chickerell_2 = pylon.Bus("Chickerell 2", v_base=400.0)
 axminster = pylon.Bus("Axminster", v_base=400.0)
 exeter = pylon.Bus("Exeter", v_base=400.0)
 langage = pylon.Bus("Langage", v_base=400.0)
-landulph = pylon.Bus("Landulph", v_base=400.0)
+landulph_1 = pylon.Bus("Landulph 1", v_base=400.0)
+landulph_2 = pylon.Bus("Landulph 2", v_base=400.0)
 indian_queens = pylon.Bus("Indian Queens", v_base=400.0)
 taunton = pylon.Bus("Taunton", v_base=400.0)
 hinkley_point = pylon.Bus("Hinkley Point", v_base=400.0)
@@ -72,7 +78,8 @@ kemsley = pylon.Bus("Kemsley", v_base=400.0)
 rowdown = pylon.Bus("Rowdown", v_base=400.0)
 beddington = pylon.Bus("Beddington", v_base=400.0)
 littlebrook = pylon.Bus("Littlebrook", v_base=400.0)
-grain = pylon.Bus("Grain", v_base=400.0)
+grain_1 = pylon.Bus("Grain 1", v_base=400.0)
+grain_2 = pylon.Bus("Grain 2", v_base=400.0)
 kingsnorth = pylon.Bus("Kingsnorth", v_base=400.0)
 northfleet_east = pylon.Bus("Northfleet East", v_base=400.0)
 barking = pylon.Bus("Barking", v_base=400.0)
@@ -85,8 +92,10 @@ bramford = pylon.Bus("Bramford", v_base=400.0)
 sizewell = pylon.Bus("Sizewell", v_base=400.0)
 norwich_main = pylon.Bus("Norwich Main", v_base=400.0)
 pelham = pylon.Bus("Pelham", v_base=400.0)
+rye_house = pylon.Bus("Rye House", v_base=400.0)
 wymondley_main = pylon.Bus("Wymondley Main", v_base=400.0)
-sundon = pylon.Bus("Sundon", v_base=400.0)
+sundon_1 = pylon.Bus("Sundon 1", v_base=400.0)
+sundon_2 = pylon.Bus("Sundon 2", v_base=400.0)
 east_claydon = pylon.Bus("East Claydon", v_base=400.0)
 enderby = pylon.Bus("Enderby", v_base=400.0)
 ratcliffe_on_soar = pylon.Bus("Ratcliffe on Soar", v_base=400.0)
@@ -116,9 +125,6 @@ osbaldwick = pylon.Bus("Osbaldwick", v_base=400.0)
 norton = pylon.Bus("Norton", v_base=400.0)
 lackenby = pylon.Bus("Lackenby", v_base=400.0)
 
-bus = pylon.Bus("", v_base=400.0)
-
-
 spt_400 =[
     pylon.Branch(hunterston, inverkip),
     pylon.Branch(inverkip, devol_moor),
@@ -133,25 +139,156 @@ spt_400 =[
 ]
 
 ngt_400 = [
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
-    pylon.Branch(),
+    pylon.Branch(gretna, harker, 1),
+    pylon.Branch(harker, hutton_1, 1),
+    pylon.Branch(harker, hutton_2, 1),
+    pylon.Branch(hutton_1, heysham, 1),
+    pylon.Branch(hutton_2, heysham, 1),
+    pylon.Branch(hutton_1, penwortham, 1),
+    pylon.Branch(hutton_2, penwortham, 1),
+    pylon.Branch(heysham, penwortham, 2),
+    pylon.Branch(penwortham, padham, 1),
+    pylon.Branch(penwortham, dains, 1),
+    pylon.Branch(padham, kearsley, 1),
+    pylon.Branch(kearsley, dains, 1),
+    pylon.Branch(dains, deeside, 2),
+    pylon.Branch(deeside, frodsham, 2),
+    pylon.Branch(deeside, pentir, 2),
+    pylon.Branch(pentir, dinorwig, 1),
+    pylon.Branch(pentir, wylfa, 2),
+    pylon.Branch(deeside, trawsfynydd, 1),
+    pylon.Branch(deeside, legacy, 2),
+    pylon.Branch(legacy, ironbridge, 2),
+    pylon.Branch(ironbridge, rugeley, 1),
+    pylon.Branch(rugeley, drakelow, 1),
+    pylon.Branch(drakelow, cellarhead, 2),
+    pylon.Branch(cellarhead, dains, 1),
+    pylon.Branch(cellarhead, macclesfield, 1),
+    pylon.Branch(dains, macclesfield, 1),
+    pylon.Branch(drakelow, hams_hall, 1),
+    pylon.Branch(hams_hall, willington_east, 1),
+    pylon.Branch(hams_hall, feckenham, 1),
+    pylon.Branch(feckenham, walham, 1),
+    pylon.Branch(walham, pembroke, 1),
+    pylon.Branch(walham, rassau, 1),
+    pylon.Branch(rassau, cilfynydd, 1),
+    pylon.Branch(cilfynydd, swansea_north, 1),
+    pylon.Branch(cilfynydd, pembroke, 1),
+    pylon.Branch(cilfynydd, pembroke, 1),
+    pylon.Branch(swansea_north, pembroke, 1),
+    pylon.Branch(cilfynydd, imperial_park, 1),
+    pylon.Branch(cilfynydd, seabank, 1),
+    pylon.Branch(imperial_park, melksham, 1),
+    pylon.Branch(seabank, melksham, 1),
+    pylon.Branch(melksham, minety, 2),
+    pylon.Branch(minety, cowley, 1),
+    pylon.Branch(minety, feckenham, 1),
+    pylon.Branch(cowley, walham, 1),
+    pylon.Branch(cowley, didcot, 2),
+    pylon.Branch(didcot, bramley, 2),
+    pylon.Branch(bramley, fleet, 2),
+    pylon.Branch(fleet, lovedean, 2),
+    pylon.Branch(lovedean, botley_wood, 1),
+    pylon.Branch(lovedean, fawley, 1),
+    pylon.Branch(botley_wood, fawley, 1),
+    pylon.Branch(fawley, marchwood, 1),
+    pylon.Branch(marchwood, nursling, 1),
+    pylon.Branch(nursling, lovedean, 1),
+    pylon.Branch(fawley, mannington, 1),
+    pylon.Branch(lovedean, mannington, 1),
+    pylon.Branch(mannington, chickerell_1, 1),
+    pylon.Branch(mannington, chickerell_2, 1),
+    pylon.Branch(chickerell_1, exeter, 1),
+    pylon.Branch(chickerell_2, axminster, 1),
+    pylon.Branch(exeter, langage, 2),
+    pylon.Branch(langage, landulph_1, 1),
+    pylon.Branch(langage, landulph_2, 1),
+    pylon.Branch(landulph_1, indian_queens, 1),
+    pylon.Branch(landulph_2, indian_queens, 1),
+    pylon.Branch(indian_queens, taunton, 2, online=False),
+    pylon.Branch(exeter, hinkley_point, 2),
+    pylon.Branch(hinkley_point, melksham, 2),
+    pylon.Branch(melksham, bramley, 2),
+    pylon.Branch(lovedean, bolney, 2),
+    pylon.Branch(bolney, ninfield, 2),
+    pylon.Branch(ninfield, dugeness, 2),
+    pylon.Branch(dugeness, sellindge, 2),
+    pylon.Branch(sellindge, canterbury_north, 2),
+    pylon.Branch(canterbury_north, kemsley, 2),
+    pylon.Branch(kemsley, rowdown, 1),
+    pylon.Branch(littlebrook, kemsley, 1),
+    pylon.Branch(rowdown, littlebrook, 1),
+    pylon.Branch(rowdown, beddington, 1),
+    pylon.Branch(kemsley, grain_1, 1),
+    pylon.Branch(kemsley, grain_2, 1),
+    pylon.Branch(grain_1, grain_2, 1, b=0.9),
+    pylon.Branch(grain_1, coryton, 1),
+    pylon.Branch(grain_2, kingsnorth, 1),
+    pylon.Branch(kingsnorth, northfleet_east, 2),
+    pylon.Branch(northfleet_east, barking, 2),
+    pylon.Branch(barking, west_ham, 2),
+    pylon.Branch(west_ham, city_road, 2),
+    pylon.Branch(city_road, unknown, 2),
+    pylon.Branch(unknown, sundon_1, 1),
+    pylon.Branch(kingsnorth, rayleigh_main, 1),
+    pylon.Branch(rayleigh_main, coryton, 1),
+    pylon.Branch(rayleigh_main, bramford),
+    pylon.Branch(bramford, sizewell, 1),
+    pylon.Branch(bramford, sizewell, 1),
+    pylon.Branch(sizewell, norwich_main, 1),
+    pylon.Branch(sizewell, pelham, 1),
+    pylon.Branch(bramford, norwich_main, 1),
+    pylon.Branch(norwich_main, walpole, 2),
+    pylon.Branch(rayleigh_main, pelham, 1),
+    pylon.Branch(pelham, rye_house, 1),
+    pylon.Branch(pelham, wymondley_main, 1),
+    pylon.Branch(wymondley_main, eaton_socon, 1),
+    pylon.Branch(eaton_socon, cottam, 1),
+    pylon.Branch(wymondley_main, cottam, 1),
+    pylon.Branch(wymondley_main, sundon_2, 1),
+    pylon.Branch(pelham, sundon_2, 1),
+    pylon.Branch(sundon_1, east_claydon, 1),
+    pylon.Branch(sundon_1, cowley, 1),
+    pylon.Branch(east_claydon, cowley, 1),
+    pylon.Branch(east_claydon, enderby, 2),
+    pylon.Branch(enderby, ratcliffe_on_soar, 2),
+    pylon.Branch(sundon_1, grendon, 1),
+    pylon.Branch(sundon_2, grendon, 1),
+    pylon.Branch(grendon, staythorpe, 1),
+    pylon.Branch(grendon, west_burton, 1),
+    pylon.Branch(staythorpe, cottam, 1),
+    pylon.Branch(staythorpe, cottam, 1),
+    pylon.Branch(staythorpe, ratcliffe_on_soar, 1),
+    pylon.Branch(ratcliffe_on_soar, high_marnham, 1),
+    pylon.Branch(high_marnham, west_burton, 1),
+    pylon.Branch(west_burton, walpole, 1),
+    pylon.Branch(walpole, burwell_main, 2),
+    pylon.Branch(walpole, spalding_north, 1),
+    pylon.Branch(spalding_north, keadby, 1),
+    pylon.Branch(keadby, cottam, 2),
+    pylon.Branch(keadby, west_burton, 1),
+    pylon.Branch(keadby, grimsby_west, 1),
+    pylon.Branch(grimsby_west, south_humber_bank, 1),
+    pylon.Branch(south_humber_bank, killingholme, 1),
+    pylon.Branch(killingholme, humber_refinery, 1),
+    pylon.Branch(killingholme, creyke_beck, 1),
+    pylon.Branch(humber_refinery, creyke_beck, 1),
+    pylon.Branch(keadby, creyke_beck, 1),
+    pylon.Branch(keadby, drax, 2),
+    pylon.Branch(keadby, brinsworth, 1),
+    pylon.Branch(brinsworth, thorpe_marsh, 1),
+    pylon.Branch(thorpe_marsh, drax, 1),
+    pylon.Branch(thorpe_marsh, eggsborough, 1),
+    pylon.Branch(thorpe_marsh, stocksbridge, 1),
+    pylon.Branch(stocksbridge, macclesfield, 1),
+    pylon.Branch(eggsborough, monk_fryston, 1),
+    pylon.Branch(monk_fryston, padham, 1),
+    pylon.Branch(eggsborough, drax, 2),
+    pylon.Branch(drax, thornton, 2),
+    pylon.Branch(thornton, creyke_beck, 2),
+    pylon.Branch(thornton, osbaldwick, 2),
+    pylon.Branch(osbaldwick, norton, 2),
+    pylon.Branch(thornton, lackenby, 2),
+    pylon.Branch(norton, lackenby, 1),
+    pylon.Branch(stella_west, eccles, 2)
 ]
