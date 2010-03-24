97a042999cebdcfa5fc4ec110abae67c40ee3c91
Richard Lincoln <r.w.lincoln@gmail.com>
2010-03-24 16:45:48 +0000

    Fixing PIPS example.

--- examples/nlp.py
+++ examples/nlp.py
@@ -3,36 +3,34 @@ __author__ = 'Richard Lincoln, r.w.lincoln@gmail.com'
 """ This example demonstrates how to use the Python Interior Point Solver using
 the problem from http://en.wikipedia.org/wiki/Nonlinear_programming. """
 
-from numpy import r_, c_
+from numpy import array, r_, float64, dot
+from scipy.sparse import csr_matrix
 from pips import pips
 
 def f2(x):
     f = -x[0] * x[1] - x[1] * x[2]
     df = -r_[x[1], x[0] + x[2], x[1]]
     # actually not used since 'hess_fcn' is provided
-    d2f = -c_[ r_[0, 1, 0], r_[1, 0, 1], r_[0, 1, 0] ]
+    d2f = -array([[0, 1, 0], [1, 0, 1], [0, 1, 0]], float64)
     return f, df, d2f
 
 def gh2(x):
-    h = c_[ r_[ 1, -1, 1], r_[1, 1, 1] ] * x**2 + r_[-2, -10]
-    dh = 2 * c_[ r_[x[0], x[0]], r_[-x[1], x[1]], r_[x[2], x[2]] ]
-    g = None
+    h = dot(array([ [1, -1, 1], [1, 1, 1] ]), x**2) + array([-2.0, -10.0])
+    dh = 2 * csr_matrix( array([ [x[0], x[0]], [-x[1], x[1]], [x[2], x[2]] ]) )
+    g = array([])
     dg = None
     return h, g, dh, dg
 
 def hess2(x, lam):
     mu = lam["ineqnonlin"]
-    Lxx = c_[ r_[2 * r_[1, 1] * mu, -1, 0],
-              r_[-1, 2 * r_[-1, 1] * mu, -1],
-              r_[0, -1, 2 * r_[1, 1] * mu] ]
+    Lxx = csr_matrix( array([ r_[dot(2 * array([1, 1]), mu), -1, 0],
+                              r_[-1, dot(2 * array([-1, 1]), mu), -1],
+                              r_[0, -1, dot(2 * array([1, 1]), mu)] ]) )
     return Lxx
 
-problem = {"f_fcn": f2,
-           "gh_fcn": gh2,
-           "hess_fcn": hess2,
-           "x0": r_[1, 1, 0],
-           "opt": {"verbose": True}}
+x0 = array([1, 1, 0], float64)
+opt = {"verbose": True}
 
-x, f, exitflag, output, lmbda = pips(problem)
+solution = pips(f2, x0, gh_fcn=gh2, hess_fcn=hess2, opt=opt)
 
-assert f == -7.0711
+print solution["output"]["iterations"], "F:", solution["x"], solution["f"]
