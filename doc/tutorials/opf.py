8d6940952621a20b3857eec713856ca62cfe90d5
Richard Lincoln <r.w.lincoln@gmail.com>
2010-03-31 15:46:42 +0100

    Updating documentation.

--- doc/tutorials/opf.py
+++ doc/tutorials/opf.py
@@ -4,30 +4,36 @@
 # Author: Richard Lincoln, r.w.lincoln@gmail.com
 #------------------------------------------------------------------------------
 
-__author__ = 'Richard Lincoln, r.w.lincoln@gmail.com'
+""" This tutorial provides a guide for solving an Optimal Power Flow problem
+using Pylon.
 
-import sys
+First import the necessary components from Pylon."""
+
 from pylon import Case, Bus, Branch, Generator, OPF
 
-""" This tutorial provides a guide for solving an Optimal Power
-Flow problem using Pylon.
+""" Import "sys" for writing to stdout. """
+import sys
 
-First create two generators with different costs: """
+""" Create two generators, specifying their marginal cost. """
+bus1 = Bus(p_demand=100.0)
+g1 = Generator(bus1, p_min=0.0, p_max=80.0, p_cost=(0.0, 6.0, 0.0))
+bus2 = Bus()
+g2 = Generator(bus2, p_min=0.0, p_max=60.0, p_cost=(0.0, 9.0, 0.0))
 
-g1 = Generator(p_min=0.0, p_max=80.0, p_cost=(0.0, 6.0, 0.0))
-g2 = Generator(p_min=0.0, p_max=60.0, p_cost=(0.0, 9.0, 0.0))
-
-
-""" Add these to a network with the desired connectivity. """
-
-bus1 = Bus(generators=[g1], p_demad=100.0)
-bus2 = Bus(generators=[g2])
+""" Connect the two generator buses """
 line = Branch(bus1, bus2, r=0.05)
-case = Case(buses=[bus1, bus2], branches=[line])
+
+""" and add it all to a case. """
+case = Case(buses=[bus1, bus2], branches=[line], generators=[g1, g2])
+
+""" Linearised DC optimal power flow """
+dc = True
+
+""" or non-linear AC optimal power flow may be selected. """
+dc = False
 
 """ Pass the case to the OPF routine and solve. """
+OPF(case, dc).solve()
 
-OPF(case, True).solve()
-
-""" View the results as ReStructuredText tables. """
+""" View the results as ReStructuredText. """
 case.save_rst(sys.stdout)
