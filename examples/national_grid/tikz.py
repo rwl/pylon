6319e0a70a815c7e2147492a99d9ea06044958db
Richard W. Lincoln <r.w.lincoln@gmail.com>
2010-08-09 22:34:21 +0100

    Adding write_line_strings tikz function.

--- examples/national_grid/tikz.py
+++ examples/national_grid/tikz.py
@@ -1,5 +1,6 @@
 __author__ = 'Richard Lincoln, r.w.lincoln@gmail.com'
 
+import os.path
 import math
 import zipfile
 import xml.etree.ElementTree as ET
@@ -7,6 +8,10 @@ import xml.etree.ElementTree as ET
 NS_KML = "http://www.opengis.net/kml/2.2"
 
 MERC_SCALE = 1e5
+
+NGT_PATH = "national_grid.kmz"
+NGT_PATH_PATH = "ngt_path.kml"
+OUT_PATH = "/home/rwl/latex/workspace/tikz/tikz/"
 
 # Mercator projection algorithms by Paulo Silva
 def deg_rad(ang):
@@ -33,7 +38,18 @@ def merc_y(lat):
     y=0-r_major*math.log(ts)
     return y
 
-def write_linear_rings(kmz_path, tikz_fd):
+def write_coords(tikz_fd, coords):
+    frags = coords.split(" ")
+    for i, c in enumerate(frags):
+        if len(c):
+            if i != 0:
+                tikz_fd.write(" -- ")
+#            print tuple([float(a) for a in c.split(",")])
+            x, y, _ = [float(a) for a in c.split(",")]
+            tikz_fd.write('(%.16f, %.16f)' % (merc_x(x) / MERC_SCALE, merc_y(y) / MERC_SCALE))
+    tikz_fd.write(";\n")
+
+def write_linear_rings(kmz_path, tikz_fd, style="-"):
     file = zipfile.ZipFile(kmz_path, "r")
     tree = ET.parse(file.open("doc.kml")) # Python 2.6
 
@@ -43,16 +59,21 @@ def write_linear_rings(kmz_path, tikz_fd):
 
     for lr in root.findall(".//{%s}LinearRing" % NS_KML):
         coords = lr.findtext(".//{%s}coordinates" % NS_KML)
-        tikz_fd.write("\draw[-] ")
-        frags = coords.split(" ")
-        for i, c in enumerate(frags):
-            if len(c):
-#                print tuple([float(a) for a in c.split(",")])
-                x, y, _ = [float(a) for a in c.split(",")]
-                tikz_fd.write('(%.16f, %.16f)' % (merc_x(x) / MERC_SCALE, merc_y(y) / MERC_SCALE))
-                if not i == len(frags) - 2:
-                    tikz_fd.write(" -- ")
-        tikz_fd.write(";\n")
+        tikz_fd.write("\draw[%s] " % style)
+        write_coords(tikz_fd, coords)
+
+def write_line_strings(kml_path, tikz_fd, defaultStyle="-"):
+    tree = ET.parse(kml_path)
+    root = tree.getroot()
+
+    for pm in root.findall(".//{%s}Placemark" % NS_KML):
+        style = pm.findtext("./{%s}styleUrl" % NS_KML)
+        if style is None:
+            style = defaultStyle
+        ls = pm.find("./{%s}LineString" % NS_KML)
+        coords = ls.findtext(".//{%s}coordinates" % NS_KML)
+        tikz_fd.write("\draw[%s] " % style)
+        write_coords(tikz_fd, coords)
 
 def write_generators(kmz_path, tikz_fd, gen_dir="gen"):
     file = zipfile.ZipFile(kmz_path, "r")
@@ -80,30 +101,46 @@ def write_generators(kmz_path, tikz_fd, gen_dir="gen"):
 #                    (mx, my))
                 tikz_fd.write('\\node [cross out,draw=black!60,minimum width=2pt,minimum height=2pt,inner sep=0pt] at (%.16f, %.16f) {};\n' % (mx, my))
 
+def write_boundary_tex():
+    simple = False
+    boundaries_path = os.path.join(OUT_PATH, "boundaries.tex")
+
+    if simple:
+        uk_path = "data/UK-simplified.kmz"
+        ireland_path = "data/Ireland-simplified.kmz"
+    else:
+        uk_path = "data/UK.kmz"
+        ireland_path = "data/Ireland.kmz"
+
+    boundaries_fd = open(boundaries_path, "w+b")
+    boundaries_fd.write("% UK\n")
+    boundaries_fd.write("\\tikzstyle{uk} = [-];\n")
+    boundaries_fd.write("\\tikzstyle{ire} = [-];\n")
+    write_linear_rings(uk_path, boundaries_fd, style="uk")
+    boundaries_fd.write("\n% Ireland\n")
+    write_linear_rings(ireland_path, boundaries_fd, style="ire")
+    boundaries_fd.close()
+
+def write_branch_tex():
+    branches_path = os.path.join(OUT_PATH, "branches.tex")
+
+    branches_fd = open(branches_path, "w+b")
+    branches_fd.write("% Lines\n")
+    write_line_strings(NGT_PATH_PATH, branches_fd, "other")
+    branches_fd.close()
+
+def write_generator_tex():
+    generators_path = os.path.join(OUT_PATH, "generators.tex")
+
+    generators_fd = open(generators_path, "w+b")
+    generators_fd.write("% Generators\n")
+    write_generators(NGT_PATH, generators_fd)
+    generators_fd.close()
 
 def main():
-    tikz_path = "/home/rwl/latex/workspace/tikz/tikz/boundaries.tex"
-#    uk_path = "data/UK.kmz"
-#    ireland_path = "data/Ireland.kmz"
-    uk_path = "data/UK-simplified.kmz"
-    ireland_path = "data/Ireland-simplified.kmz"
-    gen_path = "national_grid.kmz"
-
-    tikz_fd = open(tikz_path, "w+b")
-    tikz_fd.write("% UK\n")
-    write_linear_rings(uk_path, tikz_fd)
-    tikz_fd.close()
-
-    tikz_fd = open(tikz_path, "a+b")
-    tikz_fd.write("\n% Ireland\n")
-    write_linear_rings(ireland_path, tikz_fd)
-    tikz_fd.close()
-
-    tikz_fd = open(tikz_path, "a+b")
-    tikz_fd.write("\n% Generators\n")
-    write_generators(gen_path, tikz_fd)
-    tikz_fd.close()
-
+#    write_boundary_tex()
+    write_branch_tex()
+#    write_generator_tex()
 
 if __name__ == "__main__":
     main()
